6c0c7d7015b539754e46ae295ee45faf55a21ca2
Thomas Cherry <thomas.a.cherry@nasa.gov>
2018-10-25 16:29:48 -0400

    changes from work

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,7 +3,12 @@ defaults write com.apple.appstore ShowDebugMenu -bool true
 defaults write com.apple.screencapture type -string "png"
 defaults write com.apple.finder QuitMenuItem -bool true
 
+defaults -currentHost write .GlobalPreferences com.apple.mouse.swapLeftRightButton -bool false
+
 chflags nohidden ~/Library/
 
 defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type="spacer-tile";}'
 
+defaults write com.apple.dock persistent-others -array-add '{"tile-data" = {"list-type" = 1;}; "tile-type" = "recents-tile";}'
+
+killall Dock
